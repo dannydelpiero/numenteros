aebfcaa18f3bef64af0dcf7ed60b926530d6ce5c
Danny Del Pezo <ninedelpezo@gmail.com>
2016-03-27 10:42:51 -0500

    Prgrama dado

--- scripts.js
+++ scripts.js
@@ -8,9 +8,17 @@ var segundosPorDia=segundosPorMinuto *minutosPorHora * horasPorDia;
 document write("<h1>Calculadora del tiempo</h1><p> Hay"+segundosPorDia  +"en un dia</p>");
 console.log("fin del programa");
 */
+
+/*
 console.log("inicio del programa");
 var peliculasVistas = prompt("Cuantas peliculas as visto este mes?");
 var seriesVistas = prompt("Cuantas series as visto este mes?");
 var tiempoPantalla = parseInt(peliculasVistas)+parseInt(seriesVistas);
 alert ('Wow! Has visto ' + tiempoPantalla+" veces minimo la pantalla");
+console.log("fin del programa");
+*/
+
+console.log("inicio del programa");
+var dado=Math.floor(Math.ramdom()*6)+1;
+alert ("Dado dice: " + dado);
 console.log("fin del programa");
