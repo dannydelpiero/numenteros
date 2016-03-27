374cf9d7f1e666903e42dd5bad25a9f30a611df6
Danny Del Pezo <ninedelpezo@gmail.com>
2016-03-27 09:13:29 -0500

    Trabajando con numeros enteros funcion parseint

---
+++ scripts.js
@@ -0,0 +1,16 @@
+/*
+console.log("inicio del programa");
+var segundosPorMinuto=60;
+var minutosPorHora=60;
+var horasPorDia=24;
+
+var segundosPorDia=segundosPorMinuto *minutosPorHora * horasPorDia;
+document write("<h1>Calculadora del tiempo</h1><p> Hay"+segundosPorDia  +"en un dia</p>");
+console.log("fin del programa");
+*/
+console.log("inicio del programa");
+var peliculasVistas = prompt("Cuantas peliculas as visto este mes?");
+var seriesVistas = prompt("Cuantas series as visto este mes?");
+var tiempoPantalla = parseInt(peliculasVistas)+parseInt(seriesVistas);
+alert ('Wow! Has visto ' + tiempoPantalla+" veces minimo la pantalla");
+console.log("fin del programa");
